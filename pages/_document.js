930c4289e39462d31d4bd645f78161cff61363f4
michael ciccarelli <michael@fyreapp.com>
2017-10-04 15:35:00 -0400

    adding brand assets, colors

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,9 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <meta charset='UTF-8' />
+          <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+          <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.css' />
           <link rel='stylesheet' href='/static/css/bundle.css' />
         </Head>
         <body>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import ss from './index.sass'
+import LogoSM from '../svgs/avatar-logo.svg'
+import LogoLG from '../svgs/avatar-logo-plus.svg'
 
 export const IndexPage = () => (
   <div className={ss.example}>
-    This is an example from scoped style in a outside CSS file {'<3'}
+    <LogoLG />
+    <h2>
+      H2
+    </h2>
+    <h6>
+      H6
+    </h6>
+    <p>
+      This is ans exasmplse fsromscoped style in a outside CSS file {'<3'}
+    </p>
+    <LogoSM />
   </div>
 )
 
