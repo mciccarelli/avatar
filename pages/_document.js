f860735bf2d1e7242483e09dbd569c32c0fc92d9
michael ciccarelli <michael@fyreapp.com>
2017-10-05 18:22:45 -0400

    adding favicon and meta data

--- pages/_document.js
+++ pages/_document.js
@@ -13,9 +13,11 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <title>Avatar Companies</title>
-          <meta name="description" content="" />
-          <meta name="keywords" content="" />
+          <meta name="description" content="We are focused on making equity investments in both emerging and established companies which offer proprietary, branded and scalable solutions to the mental health and wellness crisis affecting our world." />
+          <meta name="keywords" content="Addiction, Anxiety and Depression , Fitness and Nutrition, Meditation and Mindfulness, Shame and Trauma, Spirituality" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="shortcut icon" href="/static/favicon.ico" type="image/x-icon" />
+          <link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
           {styleTags}
         </Head>
         <body>
