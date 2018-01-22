64bd09e903f98f55c2dbda6ec4adacf9643acb7b
michael ciccarelli <mikecicc@gmail.com>
2018-01-22 17:17:15 -0500

    connect Contentful back-end

--- pages/index.js
+++ pages/index.js
@@ -1,135 +1,55 @@
-import React from 'react'
-import styled from 'styled-components'
-import { mq } from '../styles'
-import LogoSM from '../svgs/avatar-logo.svg'
-import LogoLG from '../svgs/avatar-logo-plus.svg'
+import React from 'react';
+import { App } from '../components';
+import contentfulClient from '../utils/contentfulClient';
 
-const Wrap = styled.div`
-  overflow: hidden;
-  text-align: center;
-  section {
-    min-height: 70vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    &:nth-child(1) {
-      min-height: 100vh;
-      justify-content: flex-start;
-    }          
-  }
-  .content {
-    margin: 0 auto;
-    width: 100%;
-    padding: 0 20%;
-    ${ mq.medium`
-      padding: 0 22%;
-    ` }    
-    ${ mq.large`
-      padding: 0;
-      max-width: var(--maxWidth);
-    ` }
-    h2 {
-      font-size: 1.35rem;
-      font-weight: 200;
-      display: block;
-      margin-bottom: 0;
-      ${ mq.medium`
-        font-size: 2rem;
-      ` }
-    }
-    p {
-      font-size: 1.125rem;
-      line-height: 1.35;
-      font-weight: 200;
-      margin-top: 0;
-      margin-bottom: 3.125rem;
-      ${ mq.medium`
-        font-size: 1.5rem;
-        line-height: 1.5;
-      ` }
-      &.about {
-        color: var(--accentColor);
-      }
-      &.areas {
-        opacity: 0.8;
-      }
-      &.contact {
-        color: var(--accentColor);
-        margin-bottom: 0;
-      }
-    }
-    a {
-      text-decoration: none;
-      color: var(--accentColor);
-      &:hover {
-        opacity: 0.8;
-      }
-    }
-  }
-  footer {
-    min-height: 30vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    padding: 30px;
-    span {
-      color: var(--mediumGray);
-      padding-right: 5px;
-      display: block;
-      ${ mq.medium`
-        padding-right: 20px;
-        display: inline-block;
-      ` }
-    }
-  }
-  .logo-lg {
-    width: 100%;
-    max-width: 1200px;
-    height: auto;
-    margin: 0 auto 3.125rem;
-  }
-  .logo-sm {
-    margin: 0 auto 20px;
-    max-width: 100px;
-    height: auto;
-    ${ mq.medium`
-      max-width: 130px;
-    ` }
-  }
-`
-
-export default () => ( 
-  <Wrap>
-    <section className="intro">  
-      <LogoLG className="logo-lg" />
+const HomePage = ({ data }) => (
+  <App>
+    <section className="intro">
       <div className="content">
         <p className="about">
-          Avatar Companies is a private partnership focused on making equity investments in the emerging fields of mental health and wellness.
+          Avatar Companies is a private partnership focused on making equity
+          investments in the emerging fields of mental health and wellness.
         </p>
       </div>
     </section>
-    <section>  
+    <section>
       <div className="content">
         <h2>Key Areas of Interest:</h2>
-        <p className="areas"> 
-          Addiction, Anxiety and Depression, Fitness and Nutrition, Meditation and Mindfulness, Shame and Trauma, Spirituality
+        <p className="areas">
+          Addiction, Anxiety and Depression, Fitness and Nutrition, Meditation
+          and Mindfulness, Shame and Trauma, Spirituality
         </p>
         <h2>Contact:</h2>
         <p className="contact">
           Kenneth Vancini, CEO <br />
-          o. <a href="tel:1-508-642-5800">1-508-642-5800</a><br /> 
-          m. <a href="tel:1-508-630-6879">1-508-630-6879</a><br />
-          <a href="mailto:kvancini@avatarcompanies.com">kvancini@avatarcompanies.com</a>
+          o. <a href="tel:1-508-642-5800">1-508-642-5800</a>
+          <br />
+          m. <a href="tel:1-508-630-6879">1-508-630-6879</a>
+          <br />
+          <a href="mailto:kvancini@avatarcompanies.com">
+            kvancini@avatarcompanies.com
+          </a>
         </p>
       </div>
     </section>
     <footer>
-      <LogoSM className="logo-sm" />
       <p className="content">
         <span>Avatar Companies, LLC</span>
         <span>12 East 49th St, 38th fl.</span>
         <span>New York, NY 10017</span>
       </p>
     </footer>
-  </Wrap>
-)
+  </App>
+);
+
+HomePage.getInitialProps = async () => {
+  const data = await contentfulClient.getEntries({
+    content_type: 'hero',
+    limit: 1,
+    order: '-sys.createdAt'
+  });
+
+  return { data };
+};
+
+export default HomePage;
---
+++ utils/contentfulClient.js
@@ -0,0 +1,8 @@
+const { createClient } = require('contentful');
+
+const contentfulClient = createClient({
+  space: `${process.env.CONTENTFUL_SPACE_ID}`,
+  accessToken: `${process.env.CONTENTFUL_ACCESS_TOKEN}`
+});
+
+module.exports = contentfulClient;
