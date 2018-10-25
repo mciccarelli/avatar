12f60b973f5fe034fb39028e9a9e9bb39c84991d
michael ciccarelli <mikecicc@gmail.com>
2018-10-25 12:19:28 -0500

    use env vars in CI/CD

--- README.md
+++ README.md
@@ -21,6 +21,12 @@ npm run start
 // pm2 start, etc..
 ```
 
+NOTE: starting next on PORT 8081, instead of 3000 (default), for running on AWS Elastic Beanstalk / EC2.
+
+```bash
+"start": "NODE_ENV=production next start -p 8081",
+```
+
 ## REQUIRED ENV VARIABLES
 
 place the following values inside an .env file at project root (un-tracked by git).
--- api/index.js
+++ api/index.js
@@ -1,12 +1,11 @@
 const { createClient } = require('contentful');
-// import getConfig from 'next/config';
-// const { publicRuntimeConfig } = getConfig();
-// const { contentfulSpaceId, contentfulAccessToken } = publicRuntimeConfig;
+import getConfig from 'next/config';
+const { publicRuntimeConfig } = getConfig();
+const { contentfulSpaceId, contentfulAccessToken } = publicRuntimeConfig;
 
 export const contentfulClient = createClient({
-  space: 'w2eolzxrklt3',
-  accessToken:
-    'b28067de98644aa290348dc8f434aad44582996cee17e23e86344745b03aaae0'
+  space: contentfulSpaceId,
+  accessToken: contentfulAccessToken
 });
 
 // get single entry by ID
